5eb294d31dc327f2d0767ba7d1bf5330903e6334
Rayen RAFFA <raffa.rayen@gmail.com>
2020-11-03 19:26:17 +0100

    get env variables

--- fetcher.py
+++ fetcher.py
@@ -1,1 +1,12 @@
+from decouple import config
+
+# Credentials setup
+API_KEY = config('API_KEY')
+CSE_ID = config('CSE_ID')
+
+# Change line below
 DORK = 'inurl:index.php?id=2 site:fr'
+
+
+print API_KEY
+print CSE_ID
