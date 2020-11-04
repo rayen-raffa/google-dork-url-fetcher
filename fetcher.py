b3124ff40650ffbaf1ac59b7cd0e46ff27ab3006
Rayen RAFFA <raffa.rayen@gmail.com>
2020-11-04 12:29:38 +0100

    add api endpoint

--- fetcher.py
+++ fetcher.py
@@ -8,5 +8,14 @@ CSE_ID = config('CSE_ID')
 DORK = 'inurl:index.php?id=2 site:fr'
 
 
+
+# Given DORK
+# encode to url
+# get json data from api
+# for each resp page
+#   for each url
+#       append te res file
+
+API_ENDPOINT = "https://www.googleapis.com/customsearch/v1?key={0}&cx={1}&q={2}".format(API_KEY,CSE_ID,ENCODED_DORK)
 print(API_KEY)
 print(CSE_ID)
